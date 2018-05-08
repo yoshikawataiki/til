a2f29cf6f24ad71d03e7fb490971aac13c4714ec
Yoshikawa Taiki <yoshikawataiki@gmail.com>
2018-05-08 15:50:39 +0900

    【修正】エラーが発生したので、webpack.config.jsを修正。packageのアップデートを行いました。

--- javascript-framework/webpack.config.js
+++ javascript-framework/webpack.config.js
@@ -32,7 +32,6 @@ module.exports = {
       {
         test: /\.scss$/,
         use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
           use: [
             'css-loader',
             'postcss-loader',
