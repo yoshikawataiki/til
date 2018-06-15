46bb4704933e53de6d3dda9714a8e39f7658e8e3
Yoshikawa Taiki <yoshikawataiki@gmail.com>
2018-06-16 00:21:11 +0900

    【修正】 メッセージ,条件分岐,配列の中身の表示などを表示しました

--- vue-tutorial/src/main.js
+++ vue-tutorial/src/main.js
@@ -4,10 +4,48 @@ import Vue from 'vue'
 
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
-new Vue({
+/* eslint-disable */
+// hello vue!
+var app1 = new Vue({
   el: '#app',
   data: {
     message: 'Hello Vue!'
+  }
+})
+// run time
+var app2 = new Vue({
+  el: '#app-2',
+  data: {
+    message: 'You loaded this page on ' + new Date().toLocaleString()
+  }
+})
+// if statement
+var app3 = new Vue({
+  el: '#app-3',
+  data: {
+    seen: true
+  }
+})
+// for statement
+var app4 = new Vue({
+  el: '#app-4',
+  data: {
+    todos: [
+      { text: 'Learn JavaScript' },
+      { text: 'Learn Vue' },
+      { text: 'Build something awesome' }
+    ]
+  }
+})
+// reverse message!
+var app5 = new Vue({
+  el: '#app-5',
+  data: {
+    message: 'Hello Vue.js!'
+  },
+  methods: {
+    reverseMessage: function () {
+      this.message = this.message.split('').reverse().join('')
+    }
   }
 })
