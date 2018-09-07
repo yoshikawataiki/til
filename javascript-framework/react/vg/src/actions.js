3dc4b6c9dfd83ff4ce5040a60e58ecbf1fa75d02
Yoshikawa Taiki <yoshikawataiki@gmail.com>
2018-09-07 15:21:43 +0900

    ソースコードの削除

--- javascript-framework/react/vg/src/actions.js
+++
@@ -1,2 +0,0 @@
-export const setTasks = tasks => ({ type: "SET_TASKS", tasks });
-export const addTask = title => ({ type: "ADD_TASK", title });
--- javascript-framework/react/vg/src/components/TodoItemList.jsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-import PropTypes from "prop-types";
-
-export const TodoItemList = props => {
-  const tasks = props.tasks.map((v, i) => <div key={i}>{v.title}</div>);
-  return <div>{tasks}</div>;
-};
-
-TodoItemList.propTypes = { tasks: PropTypes.array };
