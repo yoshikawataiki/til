58caa0b5216ce683467050586b7a7cfa42af70e5
よしかわたいき <yoshikawataiki@gmail.com>
2018-11-28 01:32:58 +0900

    update

--- golang-tutorial/ruby-tutorial/rails/railbook/app/controllers/hello_controller.rb
+++
@@ -1,9 +0,0 @@
-class HelloController < ApplicationController
-    def index
-        render plain: 'Hello, Rails World!'
-    end
-
-    def view
-        @msg = 'Hello, Rails World!'
-    end
-end
